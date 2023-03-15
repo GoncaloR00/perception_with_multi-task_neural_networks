35a24b463c8331648b5c5ae5043391551e291af6
GRibeiro00 <gribeiro@ua.pt>
2023-03-15 22:04:06 +0000

    adaptation to receive  images from a rostopic done

--- Inference/inference_manager/src/inference_node.py
+++ Inference/inference_manager/src/inference_node.py
@@ -1,47 +1,44 @@
 #!/usr/bin/python3
 from inference_class import Inference
 import cv2
-import time
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
 # import argparse
-# import rospy
 
 
-infer_function_name = 'yolopv2_module'
-model_path = '../../../models/yolopv2.pt'
+# TODO ARGPARSE
 
-inference = Inference(model_path, infer_function_name)
+class InferenceNode:
+    def __init__(self):
+        # ---------------------------------------------------
+        #   Model and inference module
+        # ---------------------------------------------------
 
-fps = 24
-# Create a VideoCapture object and read from input file
-cap = cv2.VideoCapture('../../../data/sample_qHD.mp4')
- 
-# Check if camera opened successfully
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
- 
+        # The inference module must have a output_organizer and a transforms 
+        # function and be in the inference_modules folder
+        infer_function_name = 'yolopv2_module'
+        model_path = '../../../models/yolopv2.pt'
+        self.inference = Inference(model_path, infer_function_name)
 
+        # ---------------------------------------------------
+        #   ROS
+        # ---------------------------------------------------
+        topic = 'inference/stream'
+        
+        subscriber_stream = rospy.Subscriber(topic, Image, self.InferenceCallback)
+        self.bridge = CvBridge()
 
-# Read until video is completed
-while(cap.isOpened()):
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  if ret == True:
-    inference.load_image(frame)
-    (det2d_class_list, det2d_list), (seg_classes, seg_list) = inference.infer()
-    cv2.imshow('teste', seg_list[0])
-    cv2.waitKey(int(1/fps * 1000))
-  else:
-    break
-#   time.sleep(1/fps)
-
-
-
+    def InferenceCallback(self,msg):
+        image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        self.inference.load_image(image)
+        (det2d_class_list, det2d_list), (seg_classes, seg_list) = self.inference.infer()
+        cv2.imshow('teste', seg_list[0])
+        cv2.waitKey(1)
 
 
-# inference.load_image(cv2.imread('../../../data/example.jpg'))
-
-# (det2d_class_list, det2d_list), (seg_classes, seg_list) = inference.infer()
-
-# print(seg_classes)
-# cv2.imshow('teste', seg_list[0])
-# cv2.waitKey(0)
+if __name__ == '__main__':
+    teste = InferenceNode()
+    rospy.init_node('receiver', anonymous=True)
+    rospy.spin()
