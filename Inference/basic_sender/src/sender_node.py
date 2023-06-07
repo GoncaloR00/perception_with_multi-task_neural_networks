936d86e12915057722ebba28d190da50671ba3dd
GoncaloR00 <gribeiro@ua.pt>
2023-06-07 02:14:29 +0100

    enet melhorado

--- Inference/basic_sender/src/sender_node.py
+++ Inference/basic_sender/src/sender_node.py
@@ -13,23 +13,34 @@ topic = '/cameras/frontcamera'
 image_pub = rospy.Publisher(topic,Image, queue_size=10)
 bridge = CvBridge()
 # Create a VideoCapture object and read from input file
-cap = cv2.VideoCapture('/home/gribeiro/catkin_ws/src/perception_with_multi-task_neural_networks/Inference/basic_sender/src/data/sample_qHD.mp4')
- 
+# cap = cv2.VideoCapture('/home/gribeiro/catkin_ws/src/perception_with_multi-task_neural_networks/Inference/basic_sender/src/data/sample_qHD.mp4')
+cap = cv2.VideoCapture('/home/gribeiro/catkin_ws/src/perception_with_multi-task_neural_networks/Inference/basic_sender/src/data/video2.mov')
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
  
 rospy.init_node('sender', anonymous=False)
-# Read until video is completed
-while(cap.isOpened() and not rospy.is_shutdown()):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if ret == True:
-      image_message = bridge.cv2_to_imgmsg(frame, encoding="passthrough")
-      image_message.header.stamp = rospy.Time.now()
-      image_pub.publish(image_message)
-    # Break the loop
-    else: 
-      break
-    time.sleep(1/fps)
-cv2.destroyAllWindows()
+
+# # Read until video is completed
+# while(cap.isOpened() and not rospy.is_shutdown()):
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#     if ret == True:
+#       image_message = bridge.cv2_to_imgmsg(frame, encoding="passthrough")
+#       image_message.header.stamp = rospy.Time.now()
+#       image_pub.publish(image_message)
+#     # Break the loop
+#     else: 
+#       break
+#     time.sleep(1/fps)
+# cv2.destroyAllWindows()
+
+
+# # TEMPORARIO
+
+# frame = cv2.imread("/home/gribeiro/catkin_ws/src/perception_with_multi-task_neural_networks/Inference/basic_sender/src/data/example.jpg")
+# while (not rospy.is_shutdown()):
+#   image_message = bridge.cv2_to_imgmsg(frame, encoding="passthrough")
+#   image_message.header.stamp = rospy.Time.now()
+#   image_pub.publish(image_message)
+#   time.sleep(1/fps)
--- Inference/inference_manager/src/inference_modules/ENet_module.py
+++ Inference/inference_manager/src/inference_modules/ENet_module.py
@@ -1,3 +1,4 @@
+import cv2
 import copy
 import torch
 from .cityscapes2bdd100k import cityscapes2bdd100k
@@ -21,24 +22,28 @@ def output_organizer(original_output, original_img_size, model_img_size):
     seg_classes = []
     seg_list = []
     for i in range(20):
-        if i == 2:
+        if i == 1 or i == 2 or i == 14:
             elements = predictions == i
             # if not any(elements):
             mask = (elements*255).squeeze(0).numpy().astype(np.uint8)
+            mask = cv2.resize(mask, (original_img_size[1], (original_img_size[0])))
             seg_classes.append(seg_classes_name[dataset_converter.convert(int(i))])
             seg_list.append(mask)
 
     if len(seg_classes) == 0:
         segmentations = None
     else:
-        segmentations = (seg_classes, seg_list, "instance")
+        segmentations = (seg_classes, seg_list, "semantic")
     detections = None
     return detections, segmentations
 
 def transforms(image, cuda:bool, device):
     original_img_size = (image.shape[0],image.shape[1])
-    model_img_size = (540, 960)
+    model_img_size = (512, 1024)
     img_0 = copy.deepcopy(image)
+    img_0 = cv2.resize(img_0, (model_img_size[1], model_img_size[0]))
+    cv2.imshow("original", img_0)
+    cv2.waitKey(1)
     img = torch.Tensor(img_0).permute(2, 0, 1)
     img = img/255
     img = img.unsqueeze(0)
